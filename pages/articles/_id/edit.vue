dd0b200ee13f21bd66bd7f82c7ea3341e1834d5f
依田尚悟 <>
2021-05-13 22:30:24 +0900

    Task12まで

---
+++ pages/articles/_id/edit.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>編集ページ</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const foo = 'foo'
+    console.log(foo)
+  },
+}
+</script>
---
+++ pages/articles/_id/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>記事詳細ページ</div>
+    <div>
+      <nuxt-link to="/articles/10/edit">Nuxt Link</nuxt-link>
+    </div>
+    <div>
+      <a href="/articles/10/edit">a タグ</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const foo = 'foo'
+    console.log(foo)
+  },
+}
+</script>
