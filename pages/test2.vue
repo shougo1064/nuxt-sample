5639d2c683d1db6033ef389451c81e3038c9bc66
依田尚悟 <>
2021-05-15 23:01:23 +0900

    Vue.js のライフサイクルを理解しよう

---
+++ pages/test2.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div>テスト２</div>
+
+    <v-text-field ref="number" v-model="number" />
+    <div>数値: {{ number }}</div>
+
+    <div :class="$style.container" v-for="user in users" :key="user.id">
+      <div>名前: {{ user.name }}</div>
+      <div>メールアドレス: {{ user.email }}</div>
+      <div>電話番号: {{ user.phone }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 0,
+      users: [],
+    }
+  },
+
+  created() {
+    console.log('created')
+
+    this.$axios
+      .get('http://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        this.users = response.data
+      })
+  },
+
+  mounted() {
+    console.log('mounted')
+    this.$refs.number.focus()
+  },
+
+  updated() {
+    console.log('updated')
+    console.log(this.number)
+  },
+}
+</script>
+
+<style lang="scss" module>
+.container {
+  margin: 10px 0;
+  padding: 8px;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+}
+</style>
